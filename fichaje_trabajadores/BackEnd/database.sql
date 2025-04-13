dcb3397bffbd3f636a1ebf7a2f4b43a09caf3aaa
Ruth <diarisecretescrit@gmail.com>
2025-04-13 18:13:04 +0200

    Base de dades amb taules actualitzades: ja relacionades

--- fichaje_trabajadores/BackEnd/database.sql
+++ fichaje_trabajadores/BackEnd/database.sql
@@ -4,15 +4,22 @@ CREATE TABLE fitxatge_neteja (
     id_treballador INT NOT NULL,
     id_comunitat INT NOT NULL,
     tipus_fitxatge ENUM('i', 'o') NOT NULL,  -- 'i' para entrada, 'o' para salida
-    data DATE NOT NULL,  -- Formato Día/Mes/Año
+    dataa DATE NOT NULL,  -- Formato Día/Mes/Año
     hora TIME NOT NULL,  -- Hora en formato HH:MM:SS
-    --- FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador),
-    --- FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- em guarda quan es crea la fila
+
+    FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE usuaris(
     tipus_usuari ENUM('admin','treballador','vei') NOT NULL,
-    tipus_permis ENUM('baix','mitja','alt') NOT NULL
+    tipus_permis ENUM('baix','mitja','alt') NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE treballadors (
@@ -20,7 +27,11 @@ CREATE TABLE treballadors (
     nom_i_cognom VARCHAR(255) NOT NULL,
     departament ENUM('parkings', 'comunitats', 'oficines') NOT NULL,
     correu VARCHAR(255) NOT NULL UNIQUE,
-    contrasenya VARCHAR(255) NOT NULL
+    contrasenya VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE admins (
@@ -29,11 +40,16 @@ CREATE TABLE admins (
     departament ENUM('parkings', 'comunitats', 'oficines') NOT NULL,
     correu VARCHAR(255) NOT NULL UNIQUE,
     contrasenya VARCHAR(255) NOT NULL
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE veins (
     id_vei INT AUTO_INCREMENT PRIMARY KEY,
-    id_comunitat INT NO NULL
+    id_comunitat INT NO NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE comunitats (
@@ -44,71 +60,114 @@ CREATE TABLE comunitats (
     ciutat VARCHAR(100) NOT NULL,
     codi_postal VARCHAR(10) NOT NULL,
     data_alta_empresa DATE NOT NULL,  -- Fecha de alta de la empresa
-    data_alta_app DATE NOT NULL  -- Fecha de alta en la app
+    data_alta_app timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,  -- Fecha de alta en la app
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE comunitat_placa_asignada (
     id_comunitat INT AUTO_INCREMENT PRIMARY KEY,
-    placa_num INT NO NULL
+    placa_num INT NO NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE places (
     placa_num INT AUTO_INCREMENT PRIMARY KEY,  -- Número de la plaza, que se autoincrementa
     id_treballador INT NOT NULL,  -- Referencia al trabajador que está asociado a la plaza
-    departament ENUM('parkings', 'comunitats', 'oficines') NOT NULL -- Departamento al que pertenece la plaza
-    --- FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)  -- Clave foránea que referencia la tabla treballadors
+    departament ENUM('parkings', 'comunitats', 'oficines') NOT NULL, -- Departamento al que pertenece la plaza
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE geolocalitzacio (
-    id_fitxatge INT -- aquesta hauria de ser FOREIGN KEY perquè fa referencia a la de FITXATGE_NETEJA
-    latitud DECIMAL(9,6),
-    longitud DECIMAL(9,6)
+    id_fitxatge INT NOT NULL,
+    latitud DECIMAL(9,6) NOT NULL,
+    longitud DECIMAL(9,6) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_fitxatge) REFERENCES fitxatge_neteja(id_fitxatge)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 
 CREATE TABLE incidencies (
     id_incidencia INT AUTO_INCREMENT,
-    tipus_incidencia ENUM ('1','2','3','4'), -- INT CHECK (tipus_incidencia IN (1, 2, 3, 4)) valida. En canvi enum genera error si sintrodueix un parametre fora dels predefinits
-    id_comunitat INT,
-    id_treballador INT,
-    nom_img VARCHAR(255),
-    data DATE,
-    hora TIME,
-    temps_en_gestionar INT
-);
+    tipus_incidencia ENUM ('1','2','3','4') NOT NULL, -- INT CHECK (tipus_incidencia IN (1, 2, 3, 4)) valida. En canvi enum genera error si sintrodueix un parametre fora dels predefinits
+    id_comunitat INT NOT NULL,
+    id_treballador INT NOT NULL,
+    nom_img VARCHAR(255) NOT NULL,
+    dataa DATE NOT NULL,
+    hora TIME NOT NULL,
+    temps_en_gestionar INT NOT NULL,
 
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
 
 CREATE TABLE tipus_incidencia (
     id_tipus INT AUTO_INCREMENT PRIMARY KEY,
-    descripcio VARCHAR(255),
-    temps_estimat_resoldre_incidencia INT
+    descripcio VARCHAR(255) NOT NULL,
+    temps_estimat_resoldre_incidencia INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 )
 
 ---taules sense primary key porque no se ha especificado una columna que se considere única y que sirva para identificar de forma única cada fila de la tabla.
 
 CREATE TABLE calendari_any (
-    id_treballador INT,
-    id_comunitat INT,
-    data DATE,
-    ordre INT
+    id_treballador INT NOT NULL,
+    id_comunitat INT NOT NULL,
+    dataa DATE NOT NULL, 
+    ordre INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id_treballador, id_comunitat, dataa, ordre),
+    FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE calendari_amb_festiu_actualitzat (
-    id_treballador INT,
-    id_comunitat INT,
-    data DATE,
-    ordre INT
+    id_treballador INT NOT NULL,
+    id_comunitat INT NOT NULL,
+    dataa DATE NOT NULL,
+    ordre INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id_treballador, id_comunitat, dataa, ordre),
+    FOREIGN KEY (id_treballador) REFERENCES treballadors(id_treballador)
+        ON DELETE CASCid_treballadorADE
+        ON UPDATE CASCADE
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE festius (
-    data_festiu DATE,
-    poblacio VARCHAR(255),
-    codi_postal VARCHAR(10)
+    data_festiu DATE NOT NULL,
+    poblacio VARCHAR(255) NOT NULL,
+    codi_postal VARCHAR(10) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (data_festiu, poblacio, codi_postal),
 );
 
 CREATE TABLE setmana (
-    dia_setmana ENUM ('L','M','X','J','V'),
-    tipus_treball ENUM ('E', 'V'),
-    id_comunitat INT,
-    ordre_comunitat INT
+    dia_setmana ENUM ('L','M','X','J','V','S') NOT NULL,
+    tipus_treball ENUM ('E', 'V') NOT NULL,
+    id_comunitat INT NOT NULL,
+    ordre_comunitat INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (dia_setmana, tipus_treball, id_comunitat,ordre_comunitat),
+    FOREIGN KEY (id_comunitat) REFERENCES comunitats(id_comunitat)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
